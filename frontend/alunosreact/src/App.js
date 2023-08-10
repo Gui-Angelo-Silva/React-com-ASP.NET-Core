3f2f950138e79a3709c5feceda01c80ae316a418
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-08-10 09:23:56 -0300

    ASP .NET Core - Consumindo uma Web API com React - V

--- frontend/alunosreact/src/App.js
+++ frontend/alunosreact/src/App.js
@@ -1,12 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import logoCadastro from './assets/logocadastro.png'
+
 function App() {
+
+  const baseUrl = 'https://localhost:7290/api/alunos';
+  
+  const [data, setData] = useState([]);
+
+  const pedidoGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  useEffect(() => {
+    pedidoGet();
+  })
+
   return (
     <div className="App">
+      <br />
+      <h3>Cadastro de Alunos</h3>
       <header className="App-header">
-        <h1>Guilherme.net</h1>
+        {/* <img src={logoCadastro} alt='Cadastro' /> */}
+        <button className='btn btn-sucess'>Adicionar</button>
       </header>
+      <table className='table table-bordered'>
+        <thead>
+          <th>Id</th>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>Idade</th>
+          <th>Operação</th>
+        </thead>
+        <tbody>
+          {data.map(aluno => (
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <button className='btn btn-primary'>Editar</button> {"  "}
+                <button className='btn btn-danger'>Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
