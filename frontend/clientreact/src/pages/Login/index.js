f0700d7e7d6bc8cbe489b0e88a1dc37a1c7af349
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-08-21 11:11:04 -0300

    ASP .NET Core - Consumindo uma Web API com React - XV

--- frontend/clientreact/src/pages/Login/index.js
+++ frontend/clientreact/src/pages/Login/index.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
+import api from "../../services/api";
+import { useNavigate } from 'react-router-dom'
+
 import LogoImagem from '../../assets/imagemDeLogin.png';
 
 export default function Login(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useNavigate();
+
+    async function login(event){
+        event.preventDefault();
+
+        const data = {
+            email, password
+        };
+
+        try {
+            const response = await api.post('api/Account/LoginUser', data);
+
+            localStorage.setItem('email', email);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('expiration', response.data.expiration);
+
+            history.push('/alunos');
+        }
+        catch (error) {
+            console.error('Login falhou: ' + error);
+            alert('O login falhou. Verifique suas credenciais e tente novamente.');
+        }        
+    }
+
     return(
         <div className="login-container">
             <section className="form">
                 <img src={LogoImagem} alt="Imagem do Login" className="img1" />
-                <form>
+                <form onSubmit={login}>
                     <h1>Cadastro de Alunos</h1>
-                    <input placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
+                    <input placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    
+                    <input type="password" placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    
                     <button class="button" type="submit">Login</button>
                 </form>
             </section>
